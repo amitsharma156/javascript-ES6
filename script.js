1c2c9630debfd41056b37594716bda40e2923097
Amit <sharma88.amit@gmail.com>
2019-12-03 20:31:04 +0530

    ES-6 blocks and IIFE

--- script.js
+++ script.js
@@ -1,45 +1,56 @@
-// let and const
+// // let and const
 
-// ES-5
-var name5 = 'Jane smith';
-var age5 = 23;
-name5 = 'Jane miller';
-console.log(name5);
+// // ES-5
+// var name5 = 'Jane smith';
+// var age5 = 23;
+// name5 = 'Jane miller';
+// console.log(name5);
 
-// ES-6
+// // ES-6
 
-const name6 = "Jane Smith";
-let age6 = 23;
-//name6 = "jane Miller";
-console.log(name6);
+// const name6 = "Jane Smith";
+// let age6 = 23;
+// //name6 = "jane Miller";
+// console.log(name6);
 
 
-// ES-5
+// // ES-5
 
-function driverLicensce5(passedTest){
-    if(passedTest){
-      //  console.log(firstName);
-        var firstName ='John';
-        var yearOfBirth = 1990;
+// function driverLicensce5(passedTest){
+//     if(passedTest){
+//       //  console.log(firstName);
+//         var firstName ='John';
+//         var yearOfBirth = 1990;
        
-    }
-    console.log(firstName + ', born in '+ yearOfBirth + ', officially allowed to drive car')
+//     }
+//     console.log(firstName + ', born in '+ yearOfBirth + ', officially allowed to drive car')
 
-}
-driverLicensce5(true);
+// }
+// driverLicensce5(true);
 
-// ES-6
+// // ES-6
 
-function driverLicensce6(passedTest){
-    //console.log(firstName);
-    let firstName;const yearOfBirth =  1990;
-    if(passedTest){
-        let firstName ='John';
-        const yearOfBirth = 1990;
-        console.log(firstName + ', born in '+ yearOfBirth + ', officially allowed to drive car')
-    }
+// function driverLicensce6(passedTest){
+//     //console.log(firstName);
+//     let firstName;const yearOfBirth =  1990;
+//     if(passedTest){
+//         let firstName ='John';
+//         const yearOfBirth = 1990;
+//         console.log(firstName + ', born in '+ yearOfBirth + ', officially allowed to drive car')
+//     }
   
         
 
+// }
+// driverLicensce6(true);
+
+
+//Blocks and IIFE
+ES-6
+{
+    const a =1;
+    let b= 2;
+    var c =3;
 }
-driverLicensce6(true);
+console.log(a+b);
+console.log(c);
